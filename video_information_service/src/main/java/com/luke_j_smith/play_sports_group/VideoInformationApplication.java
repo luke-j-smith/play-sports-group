853d88c78b3c2b8e62ed308b4bc2a905859c8871
Luke Smith <ls1046@my.bristol.ac.uk>
2019-03-03 11:56:34 +0000

    Create new main application class (display-file-contents)

---
+++ video_information_service/src/main/java/com/luke_j_smith/play_sports_group/VideoInformationApplication.java
@@ -0,0 +1,11 @@
+package com.luke_j_smith.play_sports_group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoInformationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VideoInformationApplication.class, args);
+    }
+}
