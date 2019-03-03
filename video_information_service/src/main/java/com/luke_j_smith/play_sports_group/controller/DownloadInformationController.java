7c0a8ac752adf5acbc0dee63f3b66d4919a0e4ae
Luke Smith <ls1046@my.bristol.ac.uk>
2019-03-03 14:38:56 +0000

    Update controller end-point to use download service (query-youtube)

--- video_information_service/src/main/java/com/luke_j_smith/play_sports_group/controller/DownloadInformationController.java
+++ video_information_service/src/main/java/com/luke_j_smith/play_sports_group/controller/DownloadInformationController.java
@@ -1,23 +1,20 @@
 package com.luke_j_smith.play_sports_group.controller;
 
-import com.luke_j_smith.play_sports_group.service.FileReaderService;
+import com.luke_j_smith.play_sports_group.service.DownloadInformationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import java.io.File;
-import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/download")
+@RequestMapping("api/v1")
 public class DownloadInformationController {
     @Autowired
-    FileReaderService fileReaderService;
+    DownloadInformationService downloadInformationService;
 
-    @RequestMapping(value = "channels", method = RequestMethod.GET)
-    public List<String> displayChannels() {
-        String channelFileLocation = "search-properties/channel_list";
-        File channelFile = fileReaderService.getFileFromResources(channelFileLocation);
-        return fileReaderService.getFileContentsLineByLine(channelFile);
+    @RequestMapping(value = "download", method = RequestMethod.GET)
+    public String displayChannels() {
+        downloadInformationService.downloadVideoInformation();
+        return "Hello";
     }
 }
