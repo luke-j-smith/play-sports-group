2a358ac0779125da49840f1bf8803b0fa5454b46
Luke Smith <ls1046@my.bristol.ac.uk>
2019-03-03 10:23:40 +0000

    Define initial requirements of file reader service (file-reader)

---
+++ video_information_service/src/main/java/com/luke_j_smith/play_sports_group/service/FileReaderServiceImpl.java
@@ -0,0 +1,19 @@
+package com.luke_j_smith.play_sports_group.service;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Implementation of the File Reader Service.
+ */
+public class FileReaderServiceImpl implements FileReaderService {
+    @Override
+    public File getFileFromResources(String fileName) {
+        return null;
+    }
+
+    @Override
+    public List<String> getFileContentsLineByLine(File file) {
+        return null;
+    }
+}
