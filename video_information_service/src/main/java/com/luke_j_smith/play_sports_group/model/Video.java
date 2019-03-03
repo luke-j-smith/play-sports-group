ed6a18efdf66fadcc54297d040029de1109e8147
Luke Smith <ls1046@my.bristol.ac.uk>
2019-03-03 17:01:05 +0000

    Update domain objects with mapping information (setup-db)

--- video_information_service/src/main/java/com/luke_j_smith/play_sports_group/model/Video.java
+++ video_information_service/src/main/java/com/luke_j_smith/play_sports_group/model/Video.java
@@ -1,15 +1,23 @@
 package com.luke_j_smith.play_sports_group.model;
 
+import javax.persistence.*;
 import java.sql.Timestamp;
 
 /**
  * Video Domain Object.
  */
+@Entity
+@Table(name = "videos")
 public class Video {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private int id;
 
+    @Column(name = "title", length = 100)
     private String title;
 
+    @Column(name = "date")
     private Timestamp date;
 
     public int getId() {
