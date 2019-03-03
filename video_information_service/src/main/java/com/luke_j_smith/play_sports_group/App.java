3deb2b6d9bf2dc335d2803fd45150c4354b02355
Luke Smith <ls1046@my.bristol.ac.uk>
2019-03-03 09:32:08 +0000

    Initialise maven project (video-information-service)
    To initialise the maven project, we used:
    
    mvn -B archetype:generate \
      -DarchetypeGroupId=org.apache.maven.archetypes \
      -DgroupId=com.luke_j_smith.play_sports_group \
      -DartifactId=video_information_service

---
+++ video_information_service/src/main/java/com/luke_j_smith/play_sports_group/App.java
@@ -0,0 +1,13 @@
+package com.luke_j_smith.play_sports_group;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
