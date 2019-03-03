7268b8470afb663a683bebd3a7d904715d83161f
Luke Smith <ls1046@my.bristol.ac.uk>
2019-03-03 23:11:52 +0000

    Use response entity as return type to video end-points (use-response-entity)
    This allows us to provide more information to the user.

--- video_information_service/src/main/java/com/luke_j_smith/play_sports_group/controller/VideoController.java
+++ video_information_service/src/main/java/com/luke_j_smith/play_sports_group/controller/VideoController.java
@@ -5,11 +5,10 @@ import com.luke_j_smith.play_sports_group.service.VideoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import javax.persistence.criteria.CriteriaBuilder;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,36 +19,42 @@ import java.util.List;
 public class VideoController {
     private Logger logger = LoggerFactory.getLogger(VideoController.class);
 
+    private static final String JSON_RESPONSE_HEADER = "application/json; charset=UTF-8";
+
     @Autowired
     VideoService videoService;
 
     @RequestMapping(value = "view-all", method = RequestMethod.GET)
-    @ResponseBody
-    public List<Video> getAllSavedVideoInformation() {
+    public ResponseEntity<List<Video>> getAllSavedVideoInformation() {
         logger.info("GET request to retrieve all videos save in the database.");
 
         List<Video> allVideos = videoService.getAllVideos();
 
         if (allVideos == null) {
             logger.info("VideoService returned null unexpectedly, returning an empty list instead.");
-            return new ArrayList<>();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
 
-        return allVideos;
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, JSON_RESPONSE_HEADER);
+
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(allVideos);
     }
 
     @RequestMapping(value = "view", method = RequestMethod.GET)
-    @ResponseBody
-    public Video getSavedVideo(@RequestParam("id") Integer id) {
+    public ResponseEntity<Video> getSavedVideo(@RequestParam("id") Integer id) {
         logger.info("GET request to retrieve all videos save in the database.");
 
         Video video = videoService.getVideo(id);
 
         if (video == null) {
             logger.info("VideoService returned null unexpectedly, returning an empty list instead.");
-            return null;
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
 
-        return video;
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_TYPE, JSON_RESPONSE_HEADER);
+
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(video);
     }
 }
